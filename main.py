4ba281f89ada494e84c2823488132458a1f9de48
Jack W Penick <krazydudeiskrazy@gmail.com>
2015-09-25 22:35:15 -0400

    add food form

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, render_template
+import random
+import string
+
+from flask import Flask, request, render_template, url_for, redirect, flash
 
 app = Flask(__name__)
 
@@ -17,16 +20,28 @@ foods = []
 def wheres_the_food():
     return render_template("wheres_the_food.html", foods=foods)
 
-@app.route("/add_food")
+@app.route("/add_food.form")
+def add_food_form():
+    return render_template("add_food_form.html")
+
+@app.route("/add_food.post", methods=["POST"])
 def add_food():
     result = Mash(
-        name=request.args.get('name'),
-        time=request.args.get('time'),
-        location=request.args.get('location'),
+        name=request.form['name'],
+        description=request.form['description'],
+        time=request.form['time'],
+        location=request.form['location'],
     )
     foods.append(result)
-    return "success!!!"
+    flash("Successfully added a new food event.")
+    return redirect(url_for('wheres_the_food'))
 
+def random_string(N):
+    # Source: StackOverflow
+    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
 
 if __name__ == "__main__":
+    app.debug = True
+    app.secret_key = random_string(120)
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run()
